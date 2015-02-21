8cf574f48fe1ea3f0d18d41ed00c7eebd5ea951a
John Rapp <johnfarnes@hotmail.com>
2015-02-21 16:51:43 +0100

    Saved scene tihi

--- Game Jam/Assets/DrawRamp.cs
+++ Game Jam/Assets/DrawRamp.cs
@@ -5,9 +5,10 @@ public class DrawRamp : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		LineRenderer line = gameObject.AddComponent<LineRenderer>();
+		LineRenderer line = GetComponent<LineRenderer>();
 		line.SetVertexCount(2);
-
+		line.SetPosition (0, new Vector3(-1, 0, 0));
+		line.SetPosition (1, new Vector3(1, 0, 0));
 	}
 	
 	// Update is called once per frame
